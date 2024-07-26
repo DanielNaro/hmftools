436700102c1968b7f4e2b88af8af82178e8a7b12
Daniel Naro <dnaro@ac.upc.edu>
2024-07-26 15:53:22 +0200

    Lifted using ucsc lift interface

--- hmf-common/src/main/java/com/hartwig/hmftools/common/amber/AmberGender.java
+++ hmf-common/src/main/java/com/hartwig/hmftools/common/amber/AmberGender.java
@@ -18,10 +18,25 @@ public final class AmberGender
     private static final double MIN_BAF_PERC = 0.01;
     private static final BaseRegion PSEUDOAUTOSOMAL_REGION_V37 = new BaseRegion(2699520, 155260560);
     private static final BaseRegion PSEUDOAUTOSOMAL_REGION_V38 = new BaseRegion(2781479, 156030895);
+    //Lifted from PSEUDOAUTOSOMAL_REGION_V38 using UCSC:
+    private static final BaseRegion PSEUDOAUTOSOMAL_REGION_CHM13 =
+            new BaseRegion(2394410, 154257572);
+
 
     public static Gender determineGender(final RefGenomeVersion version, final Multimap<Chromosome,AmberBAF> chromosomeBafs)
     {
-        final BaseRegion inclusionRegion = version == RefGenomeVersion.V37 ? PSEUDOAUTOSOMAL_REGION_V37 : PSEUDOAUTOSOMAL_REGION_V38;
+        BaseRegion inclusionRegion;
+
+        if (version ==  RefGenomeVersion.V37) {
+            inclusionRegion = PSEUDOAUTOSOMAL_REGION_V37;
+        } else if (version == RefGenomeVersion.V38) {
+            inclusionRegion = PSEUDOAUTOSOMAL_REGION_V38;
+        } else if (version == RefGenomeVersion.HS1) {
+            inclusionRegion = PSEUDOAUTOSOMAL_REGION_CHM13;
+        } else {
+            inclusionRegion = null;
+            throw new IllegalArgumentException();
+        }
 
         int totalPoints = 0;
         long inclusionPoints = 0;
